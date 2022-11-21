51896196843c5ada4375eec4d2d3476a011c2255
Nathan Vahrenberg <nvahren@gmail.com>
2022-11-21 08:52:49 -0600

    Automatically solve and post to twitter (#1)

--- main.py
+++ main.py
@@ -1,31 +1,21 @@
-import random
+import os
 import string
+import time
+from enum import Enum
+
+import pyperclip
+import tweepy
+from pyvirtualdisplay import Display
+from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver import ActionChains, Keys
+from selenium.webdriver.common.by import By
 
 
-def prompt_guess():
-    guess = ""
-    while len(guess) != 5:
-        guess = input('Word guessed: \n')
-
-    return guess
-
-
-def result_valid(result):
-    if len(result) != 5:
-        return False
-
-    if set(result).issubset(set('xgy')):
-        return True
-
-    return False
-
-
-def prompt_result():
-    result = ""
-    while not result_valid(result):
-        result = input('Result (x=grey, y=yellow, g=green): \n').lower()
-
-    return result
+class State(Enum):
+    CORRECT = "correct"
+    PRESENT = "present"
+    ABSENT = "absent"
 
 
 def no_match(words, letters, letter, guess, result, i):
@@ -33,7 +23,7 @@ def no_match(words, letters, letter, guess, result, i):
     for word in words.copy():
         duplicate = False
         for j in range(5):
-            if i != j and guess[j] == letter and result[j] != "x":
+            if i != j and guess[j] == letter and result[j] != State.ABSENT:
                 duplicate = True
 
         if not duplicate and letter in word:
@@ -57,11 +47,11 @@ def full_match(words, letter, i):
 
 def filter_words(words, letters, guess, result):
     for i in range(5):
-        if result[i] == "x":
+        if result[i] == State.ABSENT:
             no_match(words, letters, guess[i], guess, result, i)
-        elif result[i] == "y":
+        elif result[i] == State.PRESENT:
             partial_match(words, guess[i], i)
-        elif result[i] == "g":
+        elif result[i] == State.CORRECT:
             full_match(words, guess[i], i)
 
 
@@ -89,32 +79,18 @@ def find_effective_words(words, letters):
         return words
 
 
-def print_effective_words(effective_words):
-    print("These words contain the most common letters remaining:")
-    for i in range(len(effective_words)):
-        print(effective_words[i].strip())
-    print("\n")
+def get_next_guess(words, letters, last_guess, last_result):
+    if len(last_result) > 0:
+        filter_words(words, letters, last_guess, last_result)
+
+    next_guesses = find_effective_words(words, letters)
+
+    return next_guesses[0]
 
 
-def print_guess(words):
-    print("This is a random sampling of remaining words:")
-    i = -1
-    command = ""
-    random_words = random.sample(words, len(words))
-    while command != "y":
-        for i in range(i + 1, i + 6):
-            if i > len(random_words) - 1:
-                print("\nthat's all!\n")
-                return
+def solve(browser):
+    board = browser.find_element(By.CSS_SELECTOR, "[class*=Board-module_board__]")
 
-            print(random_words[i].strip())
-
-        print("\n")
-        command = input("Ready to guess (y/n)? ")
-
-
-def solve():
-    result = ""
     f = open("words.txt")
     words = f.readlines()
     f.close()
@@ -123,18 +99,71 @@ def solve():
     for letter in list(string.ascii_lowercase):
         letters[letter] = 0
 
-    while result != "ggggg":
-        guess = prompt_guess()
-        result = prompt_result()
+    guess = get_next_guess(words, letters, "", [])
+    for i in range(1, 7):
+        result = []
+        print("Guessing {}".format(guess.rstrip('\n').upper()))
+        ActionChains(browser).send_keys(guess).send_keys(Keys.ENTER).perform()
+        time.sleep(5)
+        row = board.find_element(By.CSS_SELECTOR, "[aria-label='Row {}']".format(i))
+        tiles = row.find_elements(By.CSS_SELECTOR, "[aria-roledescription='tile")
+        for j in range(0, 5):
+            data_state = tiles[j].get_attribute("data-state")
+            if data_state == State.CORRECT.value:
+                result.append(State.CORRECT)
+            elif data_state == State.PRESENT.value:
+                result.append(State.PRESENT)
+            else:
+                result.append(State.ABSENT)
+        if State.ABSENT not in result and State.PRESENT not in result:
+            print("The word was {}, guessed in {} tries".format(guess.rstrip('\n').upper(), i))
+            return
+        else:
+            guess = get_next_guess(words, letters, guess, result)
 
-        filter_words(words, letters, guess, result)
-        effective_words = find_effective_words(words, letters)
 
-        print_effective_words(effective_words)
-        print_guess(words)
+def post_result_twitter(result):
+    f = open(".twitter_creds")
+    creds = f.readlines()
+    f.close()
 
-    print("That's pretty neat!")
+    auth = tweepy.OAuthHandler(creds[0].rstrip('\n'), creds[1].rstrip('\n'))
+    auth.set_access_token(creds[2].rstrip('\n'), creds[3].rstrip('\n'))
+
+    api = tweepy.API(auth)
+    api.update_status(result)
+
+
+def auto_solve():
+    display = ""
+    if os.environ.get('LOCAL_RUN') != "1":
+        display = Display()
+        display.start()
+        pyperclip.set_clipboard("xclip")
+
+    browser = webdriver.Firefox()
+
+    browser.get("https://www.nytimes.com/games/wordle/index.html")
+
+    time.sleep(2)
+
+    try:
+        browser.find_element(By.CLASS_NAME, "Modal-module_closeIcon__b4z74").click()
+    except NoSuchElementException:
+        print("No modal found")
+
+    solve(browser)
+
+    time.sleep(5)
+    browser.find_element(By.ID, "share-button").click()
+    result = pyperclip.paste()
+    print(result)
+
+    browser.close()
+    if os.environ.get('LOCAL_RUN') != "1":
+        display.stop()
+        post_result_twitter(result)
 
 
 if __name__ == '__main__':
-    solve()
+    auto_solve()
